fd55b4be00c593592183e7878551c8b9aff5b7a9
郑泳健 <yongjian.zheng@tongdun.net>
2022-05-23 11:02:05 +0800

    style: 格式化

--- src/index.js
+++ src/index.js
@@ -1,5 +1,5 @@
 /*
- * @Description: 
+ * @Description:
  * @Author: 郑泳健
  * @Date: 2022-05-16 17:55:09
  * @LastEditors: 郑泳健
@@ -9,13 +9,13 @@ import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
 import reportWebVitals from './reportWebVitals';
-import './index.css'
+import './index.css';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>
+    <React.StrictMode>
+        <App />
+    </React.StrictMode>
 );
 
 // If you want to start measuring performance in your app, pass a function
